d1f55e7d5ba1daff23afad58283fb673a99719e4
allworkdone <raj635321@gmail.com>
2024-01-17 16:28:00 +0530

    Add Button and Content components Added all the nevessary image svgs for logos

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import Button from './components/Button'
+import Content from './components/Content'
 
 export default function Home() {
   return (
     <>
       {/* <NavBar /> */}
+      <Button />
+      <Content />
       <Footer />
+
     </>
   )
 }
