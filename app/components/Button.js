39658474dbe89b3a353d1a2d0ca328dbe4689955
allworkdone <raj635321@gmail.com>
2024-01-19 01:38:20 +0530

    intellifill:Button:Update Button component styling

--- app/components/Button.js
+++ app/components/Button.js
@@ -4,7 +4,7 @@ const Button = () => {
     return (
         <>
             <div className="bg-white">
-                <div className="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
+                <div className="w-full max-w-screen-xl p-4 py-2 mx-auto lg:py-4">
                     <button className="flex justify-center gap-2">
                         <IoArrowBack className="text-2xl text-black" />
                         <a href="#" className="font-bold">Back</a>
