5d1e361f74d2b781bac5ff34af418d01e408c362
allworkdone <raj635321@gmail.com>
2024-01-19 01:37:44 +0530

    intellifill:NavBar: Adjusted some elements alignment

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -7,12 +7,12 @@ const NavBar = () => {
     return (
         <>
             <header className="bg-white">
-                <div className="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
-                    <div className='flex flex-row gap-6 justify-between'>
+                <div className="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-4">
+                    <div className='flex flex-row gap-6 justify-between pt-2'>
                         <div>
                             <Image src="/logo.png" alt="logo" width={150} height={50} />
                         </div>
-                        <div>
+                        <div className='pt-2'>
                             <ul className="text-lg my-2 gap-4 hidden lg:block md:block">
                                 <li className="inline-block mr-4">
                                     <a href="#" >Home</a>
@@ -33,7 +33,7 @@ const NavBar = () => {
                         </div>
 
                         <div className='flex flex-row gap-4'>
-                            <div>
+                            <div className='py-2'>
                                 <div className="hidden lg:flex items-center justify-center w-full h-10 pl-3 pr-2 my-1 text-gray-500 border rounded-full bg-gray-50 border-blue-500 focus:outline-none focus:border-blue-500">
                                     <button type="submit" className="inline-flex items-center justify-center w-10 h-10 text-gray-400 focus:outline-none rounded-full">
                                         <RiSearch2Line className="w-5 h-5" />
@@ -46,14 +46,14 @@ const NavBar = () => {
                                     </button>
                                 </div>
                             </div>
-                            <div>
+                            <div className='py-2'>
                                 <button className="flex justify-center my-3 gap-2">
                                     <FaRegBell className="text-2xl text-blue-500" />
                                 </button>
                             </div>
                             <div>
                                 <button className="flex justify-center gap-2 ">
-                                    <Image src="/profile.png" alt="profile" width={50} height={50} className='rounded-full' />
+                                    <Image src="/profile.png" alt="profile" width={70} height={70} className='rounded-full' />
                                 </button>
                             </div>
                         </div>
